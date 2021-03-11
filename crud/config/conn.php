1f0bdbb6584a40e024f4bb0003a0b5897d0ded0c
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-03-11 23:10:43 +0100

    updated database for heroku

--- crud/config/conn.php
+++ crud/config/conn.php
@@ -1,4 +1,14 @@
 <?php
-     $conn =  new mysqli('localhost', 'root', '', 'crud') or die($conn->connect_error);
+     
+     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+     $server = $url["host"];
+     $username = $url["user"];
+     $password = $url["pass"];
+     $db = substr($url["path"], 1);
+
+     $conn = new mysqli($server, $username, $password, $db);
+
+
      $username = $email = $image = $message = "";
 ?>
